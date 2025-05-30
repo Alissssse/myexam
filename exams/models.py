7f5d53d80c7860d6d6f4116d44de546d646db058
Твоё имя <alisa.kultasheva@yandex.ru>
2025-05-30 10:32:30 +0300

    Пункт 3: Создана модель akxam для хранения информации об экзаменах с полями title, created_at, exam_date, image, users (ManyToMany) и is_public

---
+++ exams/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class akxam(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название экзамена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    exam_date = models.DateTimeField(verbose_name='Дата проведения экзамена')
+    image = models.ImageField(upload_to='exam_images/', verbose_name='Изображение задания')
+    users = models.ManyToManyField(User, verbose_name='Пользователи')
+    is_public = models.BooleanField(default=False, verbose_name='Опубликовано')
+
+    class Meta:
+        verbose_name = 'Экзамен'
+        verbose_name_plural = 'Экзамены'
+
+    def __str__(self):
+        return self.title
