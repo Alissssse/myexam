bd2f151a371bcfcee174ac5fc30ad7bd3a76d3c8
Твоё имя <alisa.kultasheva@yandex.ru>
2025-05-30 10:48:43 +0300

    Пункт 4: Настроена административная часть для модели akxam (поиск, фильтры, редактирование M2M)

--- exams/admin.py
+++ exams/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import akxam
 
-# Register your models here.
+@admin.register(akxam)
+class akxamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'exam_date', 'is_public')
+    list_filter = ('is_public', 'created_at')
+    search_fields = ('title', 'users__email')
+    date_hierarchy = 'exam_date'
+    filter_horizontal = ('users',)
